6140e5ae0cf984dcf5a1f6123a7d7deffa0d7c80
killianFerrierf22012793 <killian.ferrier.pro@etu.univ-amu.fr>
2023-09-05 12:01:08 +0200

    Update

---
+++ R3.05_Prog_Syst/TP/client.cpp
@@ -0,0 +1,67 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#define MAX 80
+#define PORT 10013
+#define SA struct sockaddr
+
+void func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	for (;;) {
+		int temp;
+		bzero(buff, sizeof(buff));
+		temp = read(sockfd, buff, sizeof(buff));
+		
+	        if (temp == 0) {
+                    printf("Server Exit...\n");
+	            break; }
+        	printf("From Server : %s", buff);
+		
+	}
+}
+
+int main(int argc, char * argv[])
+{
+	int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+
+	// socket crée et verifié 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("erreur création socket...\n");
+		exit(0);
+	}
+	else
+		printf("Création du socket réussi..\n");
+	bzero(&servaddr, sizeof(servaddr));
+
+	// assigner IP, PORT
+	char *ip = argv[1];
+	char *p_str = argv[2];
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ip);
+	unsigned short p = (unsigned short) strtoul(p_str, NULL, 0);
+	servaddr.sin_port = htons(p);
+
+	// connecter le client socket au serveur socket
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
+		!= 0) {
+		printf("connexion au serveur échoué...\n");
+		exit(0);
+	}
+	else
+		printf("connexiox au serveur réussi..\n");
+
+	// function pour recupere les données
+	func(sockfd);
+
+	// fermeture du socket
+	close(sockfd);
+}
--- R3.05_Prog_Syst/TP/serveur_daytime.cpp
+++ R3.05_Prog_Syst/TP/serveur_daytime.cpp
@@ -24,11 +24,10 @@ int main(int argc, char * argv[]) {
     int sock_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in sockaddr_serveur;
-   struct sockaddr_in client;
+    struct sockaddr_in client;
     sockaddr_serveur.sin_family = AF_INET;
     sockaddr_serveur.sin_port = htons(NUM_PORT);
     sockaddr_serveur.sin_addr.s_addr = htonl(INADDR_ANY);
-    //inet_aton("172.23.217.150",& sockaddr_serveur.sin_addr);
 
     int yes = 1;
     if (setsockopt(sock_serveur, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
@@ -57,7 +56,7 @@ int main(int argc, char * argv[]) {
         if (sock_client == -1)
             exitErreur("accept");
 
-	cout << " Le client avec l'ip " << inet_ntoa(client.sin_addr) << " s'est connecté avec le port : " << ntohs(client.sin_port)<<endl;
+	cout << "Le client avec l'ip " << inet_ntoa(client.sin_addr) << " s'est connecté avec le port : " << ntohs(client.sin_port)<<endl;
 
         date = time(NULL);
         msg = ctime(&date);
