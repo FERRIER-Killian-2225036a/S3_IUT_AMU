9f4087471e2aea0bce0844ae5d3867352acbd250
killianFerrierf22012793 <killian.ferrier.pro@etu.univ-amu.fr>
2023-10-04 11:24:49 +0200

    Update

--- R3.03_Analyse/TP2/TP2/src/exo_un/ListSimpleSansSantinelle.java
+++ R3.03_Analyse/TP2/TP2/src/exo_un/ListSimpleSansSantinelle.java
@@ -11,10 +11,66 @@ public class ListSimpleSansSantinelle {
 	
 	public ListSimpleSansSantinelle(NodeSimple n) {
 		head = n; 
+		size++;
 	}
 	
 	public ListSimpleSansSantinelle(int elem) {
 		head = new NodeSimple(elem, null); 
+	}
+	
+	public NodeSimple getHead() {
+		return head;
+	}
+	
+	public int getHeadElem() {
+		return head.getElement();
+	}
+	
+	public void setHeadElem(int e) {
+		head.setElement(e);
+	}
+	
+	public void insertElemEnTete(int e) {
+		NodeSimple node = new NodeSimple(e, head);
+		head = node;
+		size++;
+	}
+	
+	public NodeSimple getHeadNext() {
+		return head.getNext();
+	}
+	
+	public NodeSimple getAvantDernierNode() {
+		if(size<2) {
+			return null;
+		}
+		NodeSimple courant = head.getNext(), prec = head;
+		while(courant.getNext()!=null) {
+			courant = courant.getNext();
+			prec = prec.getNext();
+		}
+		return prec;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void inverserListe() {
+		NodeSimple prec = head, cour, suiv;
+		if (prec==null || prec.getNext()==null) {
+			return;
+		}
+		cour=prec.getNext();
+		suiv=cour.getNext();
+		prec.setNext(null);
+		cour.setNext(prec);
+		while(suiv!=null) {
+			prec=cour;
+			cour=suiv;
+			suiv=suiv.getNext();
+			cour.setNext(prec);
+		}
 	}
 
 }
