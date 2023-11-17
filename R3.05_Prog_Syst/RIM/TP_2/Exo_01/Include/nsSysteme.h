40ae64ac10bccb8b0d0b076cd4903c826ca69430
killianFerrierf22012793 <killian.ferrier@etu.univ-amu.fr>
2023-11-17 16:32:24 +0100

    Update

--- R3.05_Prog_Syst/RIM/TP_2/Exo_01/Include/nsSysteme.h
+++ R3.05_Prog_Syst/RIM/TP_2/Exo_01/Include/nsSysteme.h
@@ -13,38 +13,41 @@
 #include <cstddef>        // size_t
 
 #include <sys/types.h>    // ssize_t
-                                                    
+
 #include <sys/stat.h>     // struct stat, stat(), fstat()
 
 #include <unistd.h>       // open(), read(), write(), close()
 
-
 #include "string.h"
 
 #include "CExc.h"
+
+#include <fcntl.h>
 
 //  Declarations des fonctions concernant les fichiers
 //  =========================================================
 
 namespace nsSysteme
 {
-    void        Stat    (const char * file_name, struct stat * buf);
+	void 	Stat	(const char * file_name, struct stat * buf);
                              // throw (CExc);
 
-    void        Close  (int fd);
+	void 	Close	(int fd);
                              // throw (CExc);
-    int         Open   (const char * pathname, int flags);
+	int 	Open	(const char * pathname, int flags);
+       	                     // throw (CExc);
+	int 	Open	(const char * pathname, int flags, ::mode_t mode);
                              // throw (CExc);
-    int         Open   (const char * pathname, int flags, ::mode_t mode);
+	std::size_t Read (int fd, void * buf, std::size_t count);
                              // throw (CExc);
-    std::size_t Read   (int fd, void * buf, std::size_t count);
+	std::size_t Write (int fd, const void * buf, std::size_t count);
                              // throw (CExc);
-    std::size_t Write  (int fd, const void * buf, std::size_t count);
-                             // throw (CExc);     
 } // nsSysteme
 
-
-
+namespace nsFctShell
+{
+	void FileCopy(const char *dest, const char *source, const size_t NbBytes);
+}
 
 //  Definitions courtes des fonctions concernant les fichiers
 //  =========================================================
@@ -52,15 +55,16 @@ namespace nsSysteme
 inline
 void nsSysteme::Stat (const char * file_name, struct stat * buf) // throw (CExc)
 {
-    if (::stat (file_name, buf))
-        throw CExc ("stat()", std::string("fichier :")+ file_name);
+	if (::stat (file_name, buf))
+		throw CExc ("stat()", std::string("fichier :")+ file_name);
 
 } // Stat()
 
 inline
 void nsSysteme::Close (int fd) // throw (CExc)
 {
-    if (::close (fd)) throw CExc ("close()", fd);
+	if (::close (fd)) 
+		throw CExc ("close()", fd);
 
 } // Close()
 
@@ -68,21 +72,46 @@ inline
 std::size_t nsSysteme::Read (int fd, void * buf, std::size_t count)
     // throw (CExc)
 {
-    ::ssize_t Res;
-    if (-1 == (Res = ::read (fd, buf, count)))
-        throw CExc ("read()", fd);
+	::ssize_t Res;
+	if (-1 == (Res = ::read (fd, buf, count)))
+		throw CExc ("read()", fd);
 
-    return Res;
+	return Res;
 
 } // Read()
 
 inline
-std::size_t nsSysteme::Write (int fd, const void * buf,
-                                     std::size_t count)
+std::size_t nsSysteme::Write (int fd, const void * buf, std::size_t count)
     // throw (CExc)
 {
-  // A completer (exo_01 du TD2
+	::ssize_t Res;
+	if (-1 == (Res = ::write (fd, buf, count)))
+		throw CExc ("write()", fd);
+
+	return Res;
 } // Write()
+
+inline
+int nsSysteme::Open (const char * pathname, int flags)
+    // throw (CExc)
+{
+	int Res;
+	if (-1 == (Res = ::open (pathname, flags)))
+		throw CExc ("open()", std::string("fichier :")+ pathname);
+
+	return Res;
+} // Open()
+
+inline
+int nsSysteme::Open (const char * pathname, int flags, ::mode_t mode)
+    // throw (CExc)
+{
+	int Res;
+	if (-1 == (Res = ::open (pathname, flags)))
+		throw CExc ("open()", std::string("fichier :")+ pathname);
+
+	return Res;
+} // Open()
 
 #endif    /* __NSSYSTEME_H__ */
 
